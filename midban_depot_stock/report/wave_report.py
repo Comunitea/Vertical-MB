515a3bf16f4d0a5240030a70dfb023983f6a18e6
Javier <javier@pexego-magoi.pxgo.es>
2014-10-23 13:58:46 +0200

    [ADD] midban_depot_stock: agrupar por lote y producto en picking, añadir al informe

--- midban_depot_stock/report/wave_report.py
+++ midban_depot_stock/report/wave_report.py
@@ -61,53 +61,27 @@ class sale_report(osv.osv):
                                  multi='mult', string='Units', readonly=True),
         'boxes': fields.function(_get_units_and_boxes, type='integer',
                                  multi='mult', string='Boxes', readonly=True),
-        # 'lot_id': fields.many2one('stock.production.lot', 'Lot',
-        #                           readonly=True),
+        'lot_id': fields.many2one('stock.production.lot', 'Lot',
+                                  readonly=True),
         'wave_id': fields.many2one('stock.picking.wave', 'Wave', readonly=True)
     }
 
-    def _select(self):
-        select_str = """
-            SELECT min(SM.id) as id,
-                   SM.product_id as product_id,
-                   L.id as location_id,
-                   sum(SM.product_uom_qty) as product_qty,
-                   P.wave_id as wave_id
-        """
-        return select_str
-
-    def _from(self):
-        from_str = """
-            stock_move SM
-                INNER JOIN product_template PR on PR.id = SM.product_id
-                INNER JOIN stock_location L on L.id = PR.picking_location_id
-                INNER JOIN stock_picking P on P.id = SM.picking_id
-        """
-        return from_str
-
-    def _group_by(self):
-        group_by_str = """
-            GROUP BY SM.product_id,
-                     L.id,
-                     P.wave_id
-        """
-        return group_by_str
-
     # def _select(self):
     #     select_str = """
-    #         SELECT min(Q.id) as id,
-    #                Q.product_id as product_id,
-    #                LOT.id as lot_id,
-    #                LOC.id as location_id,
-    #                sum(Q.qty) as product_qty,
+    #         SELECT min(SM.id) as id,
+    #                SM.product_id as product_id,
+    #                L.id as location_id,
+    #                sum(SM.product_uom_qty) as product_qty,
     #                P.wave_id as wave_id
     #     """
     #     return select_str
 
     # def _from(self):
     #     from_str = """
-    #         stock_pack_operation OP
-    #             INNER JOIN stock_move_operation_link MOL on MOL.operation_id = OP.product_id
+    #         stock_move SM
+    #             INNER JOIN product_template PR on PR.id = SM.product_id
+    #             INNER JOIN stock_location L on L.id = PR.picking_location_id
+    #             INNER JOIN stock_picking P on P.id = SM.picking_id
     #     """
     #     return from_str
 
@@ -115,17 +89,98 @@ class sale_report(osv.osv):
     #     group_by_str = """
     #         GROUP BY SM.product_id,
     #                  L.id,
-    #                  LOT.id,
     #                  P.wave_id
     #     """
     #     return group_by_str
+
+    # def init(self, cr):
+    #     # self._table = sale_report
+    #     tools.drop_view_if_exists(cr, self._table)
+    #     cr.execute("""CREATE or REPLACE VIEW %s as (
+    #         %s
+    #         FROM ( %s )
+    #         %s
+    #         UNION
+    #         %s
+    #         FROM %s
+    #         %s
+    #         )""" % (self._table, self._select(), self._from(),
+    #                 self._group_by()))
+
+    def _select1(self):
+        select_str = """
+            SELECT min(OP.id) as id,
+                   Q.product_id as product_id,
+                   Q.lot_id as lot_id,
+                   OP.location_id ,
+                   sum(Q.qty) as product_qty,
+                   W.id as wave_id
+        """
+        return select_str
+
+    def _from1(self):
+        from_str = """
+            stock_quant Q
+                INNER JOIN stock_quant_package PA on PA.id = Q.package_id
+                INNER JOIN stock_pack_operation OP on OP.package_id = PA.id
+                INNER JOIN stock_picking P on P.id = OP.picking_id
+                INNER JOIN stock_picking_wave W on W.id = P.wave_id
+            WHERE OP.product_id is null
+        """
+        return from_str
+
+    def _group_by1(self):
+        group_by_str = """
+            GROUP BY
+                Q.product_id,
+                Q.lot_id,
+                OP.location_id,
+                W.id
+        """
+        return group_by_str
+
+    def _select2(self):
+        select_str = """
+            SELECT min(OP.id) as id,
+                   OP.product_id as product_id,
+                   OP.lot_id as lot_id,
+                   OP.location_id ,
+                   sum(OP.product_qty) as product_qty,
+                   W.id as wave_id
+        """
+        return select_str
+
+    def _from2(self):
+        from_str = """
+            stock_pack_operation OP
+                INNER JOIN stock_picking P on P.id = OP.picking_id
+                INNER JOIN stock_picking_wave W on W.id = P.wave_id
+            WHERE OP.product_id is not null
+        """
+        return from_str
+
+    def _group_by2(self):
+        group_by_str = """
+            GROUP BY
+                OP.product_id,
+                OP.lot_id,
+                OP.location_id,
+                W.id
+        """
+        return group_by_str
 
     def init(self, cr):
         # self._table = sale_report
         tools.drop_view_if_exists(cr, self._table)
         cr.execute("""CREATE or REPLACE VIEW %s as (
             %s
-            FROM ( %s )
+            FROM %s
             %s
-            )""" % (self._table, self._select(), self._from(),
-                    self._group_by()))
+            UNION
+            %s
+            FROM %s
+            %s
+            )""" % (self._table, self._select1(), self._from1(),
+                    self._group_by1(),
+                    self._select2(), self._from2(), self._group_by2()
+                    ))
--- telesale/telesale.py
+++ telesale/telesale.py
@@ -20,6 +20,7 @@
 ##############################################################################
 from openerp.osv import osv, fields
 from openerp.tools.translate import _
+from openerp.tools import float_compare
 import openerp.addons.decimal_precision as dp
 
 
@@ -80,6 +81,9 @@ class sale_order_line(osv.osv):
             context = {}
         sup = super(sale_order_line, self)
         t_product = self.pool.get('product.product')
+        product_uom_obj = self.pool.get('product.uom')
+        warning_msgs = ''
+
         res = sup.product_id_change(cr, uid, ids, pricelist, product,
                                     qty=qty, uom=uom, qty_uos=qty_uos, uos=uos,
                                     name=name, partner_id=partner_id,
@@ -100,6 +104,29 @@ class sale_order_line(osv.osv):
                 if last_id:
                     line = self.browse(cr, uid, last_id[0], context)
                     res['value']['last_price_fresh'] = line.price_unit
+            # Compare stock agains virtual_stock_conservative
+            uom_record = False
+            if uom:
+                uom_record = product_uom_obj.browse(cr, uid, uom, context=context)
+                if prod_obj.uom_id.category_id.id != uom_record.category_id.id:
+                    uom_record = False
+            if not uom_record:
+                uom_record = prod_obj.uom_id
+            compare_qty = float_compare(prod_obj.virtual_stock_conservative, qty, precision_rounding=uom_record.rounding)
+            if compare_qty == -1:
+                warn_msg = _('You plan to sell %.2f %s but you only have %.2f %s available in conservative !\nThe real stock is %.2f %s. (without reservations)') % \
+                    (qty, uom_record.name,
+                     max(0,prod_obj.virtual_stock_conservative), uom_record.name,
+                     max(0,prod_obj.qty_available), uom_record.name)
+                warning_msgs += _("Not enough stock ! : ") + warn_msg + "\n\n"
+        # import ipdb; ipdb.set_trace()
+        #update of warning messages
+        if warning_msgs:
+            warning = {
+                       'title': _('Configuration Error!'),
+                       'message' : warning_msgs
+                    }
+            res.update({'warning': warning})   
         return res
 
 
