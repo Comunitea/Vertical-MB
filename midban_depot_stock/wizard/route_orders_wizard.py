2956d02abf660446cd6c0eee42082f30092b2d9f
Javier <javier@pexego-magoi.pxgo.es>
2014-11-24 18:09:03 +0100

    [FIX]: midban_depot_stock: posible bug corregido

--- midban_depot_stock/wizard/route_orders_wizard.py
+++ midban_depot_stock/wizard/route_orders_wizard.py
@@ -53,89 +53,57 @@ class route_order_wizard(osv.TransientModel):
             context = {}
         active_ids = context.get('active_ids', [])
         t_proc = self.pool.get('procurement.order')
-        move_obj = self.pool.get('stock.move')
         t_pick = self.pool.get('stock.picking')
         order_obj = self.pool.get('sale.order')
+        t_route = self.pool.get('route')
         obj = self.browse(cr, uid, ids[0], context=context)
         new_route = obj.trans_route_id and obj.trans_route_id.id or False
+        next_dc = obj.trans_route_id and obj.trans_route_id.next_dc or 0
+        model = False
         # FOR SALE ORDERS
         if context['active_model'] == "sale.order":
-            # Update orders
-            order_obj.write(cr, uid, active_ids,
-                            {'trans_route_id': new_route},
-                            context=context)
-            p_ids = t_proc.search(cr, uid,
-                                  [('sale_line_id.order_id', 'in',
-                                    active_ids)],
-                                  context=context)
-            # Update procurement route
-            t_proc.write(cr, uid, p_ids,
-                         {'trans_route_id': new_route},
-                         context=context)
-            # Update related pickings
-            # pick_ids = []
-            for so in order_obj.browse(cr, uid, active_ids, context):
-                # pick_ids += [picking.id for picking in so.picking_ids]
-                if so.group_id:
-                    group_id = so.group_id.id
-                    proc_ids = t_proc.search(cr, uid,
-                                             [('group_id', '=', group_id)],
-                                             context=context)
-                    if proc_ids:
-                        t_proc.write(cr, uid,
-                                     {'trans_route_id': new_route},
-                                     context=context)
-                        pick_ids = t_pick.search(cr, uid,
-                                                 [('group_id', '=', group_id)],
-                                                 context=context)
-                        if pick_ids:
-                            t_pick.write(cr, uid,
-                                         {'trans_route_id': new_route},
-                                         context=context)
-            # if pick_ids:
-            #     t_pick.write(cr, uid, pick_ids,
-            #                  {'trans_route_id': new_route},
-            #                  context=context)
+            model = order_obj
         # FOR PICKINGS
         elif context['active_model'] == "stock.picking":
-            move_ids = move_obj.search(cr, uid, [('picking_id', 'in',
-                                                  active_ids)],
-                                       context=context)
-            # Update pickings
-            # t_pick.write(cr, uid, active_ids,
-            #              {'trans_route_id': new_route},
-            #              context=context)
-             # Update procurement route
-            for move in move_obj.browse(cr, uid, move_ids,
-                                        context=context):
-                move.procurement_id.write({'trans_route_id':
-                                           new_route},
-                                          context=context)
-            # Update related orders
+            model = t_pick
+        if model:
             # import ipdb; ipdb.set_trace()
-            so_ids = set()
-            for pick in t_pick.browse(cr, uid, active_ids, context):
-                if pick.sale_id:
-                    so_ids.add(pick.sale_id.id)
-                if pick.group_id:
-                    group_id = pick.group_id.id
+            for model_obj in model.browse(cr, uid, active_ids, context):
+                so_ids = set()
+                group_id = False
+                if model_obj.trans_route_id and new_route \
+                        and model_obj.trans_route_id.id == new_route:
+                    continue
+                if 'sale_id' in model_obj._columns and model_obj.sale_id:
+                    so_ids.add(model_obj.sale_id.id)
+                    group_id = model_obj.group_id and \
+                        model_obj.group_id.id or False
+                elif 'sale_id' not in model_obj._columns:
+                    so_ids.add(model_obj.id)
+                    group_id = model_obj.procurement_group_id and \
+                        model_obj.procurement_group_id.id or False
+                if group_id:
                     proc_ids = t_proc.search(cr, uid,
                                              [('group_id', '=', group_id)],
                                              context=context)
                     if proc_ids:
                         t_proc.write(cr, uid, proc_ids,
-                                     {'trans_route_id': new_route},
+                                     {'trans_route_id': new_route,
+                                      'drop_code': next_dc},
                                      context=context)
                         pick_ids = t_pick.search(cr, uid,
                                                  [('group_id', '=', group_id)],
                                                  context=context)
                         if pick_ids:
                             t_pick.write(cr, uid, pick_ids,
-                                         {'trans_route_id': new_route},
+                                         {'trans_route_id': new_route,
+                                          'drop_code': next_dc},
                                          context=context)
-            so_ids = list(so_ids)
-            if so_ids:
-                order_obj.write(cr, uid, so_ids,
-                                {'trans_route_id': obj.trans_route_id.id},
-                                context=context)
+                so_ids = list(so_ids)
+                if so_ids:
+                    order_obj.write(cr, uid, so_ids,
+                                    {'trans_route_id': obj.trans_route_id.id},
+                                    context=context)
+                if new_route:
+                    t_route.write(cr, uid, new_route, {'next_dc': next_dc + 1})
         return
