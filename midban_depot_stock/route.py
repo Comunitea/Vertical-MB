b6209b67251c72abd1df70d8fcb16e6f7600afd5
javierjcf <javier@pexego-magoi.pxgo.es>
2015-03-31 18:10:37 +0200

    [ADD]: midban_depot_stock: Nuevo modelo de rutas

--- midban_depot_stock/route.py
+++ midban_depot_stock/route.py
@@ -18,40 +18,150 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+from openerp import models, fields, api
+from openerp.tools.translate import _
+from openerp.exceptions import except_orm
 
-from openerp.osv import osv, fields
 
-
-class route(osv.Model):
-    _name = 'route'
-    _description = 'Transport Route Model'
+class route_zip(models.Model):
+    _name = 'route.zip'
+    _description = "List of Zip codes covered by route"
     _rec_name = 'code'
-    _columns = {
-        'code': fields.char('Code', size=32, required=True),
-        'name': fields.char('Name', size=255),
-        'vehicle_id': fields.many2one('stock.vehicle', 'Vehicle',
-                                      required=True),
-        'state': fields.selection([('draft', 'Draft'), ('active', 'Active')],
-                                  string="State", readonly=True),
-        'next_dc': fields.integer('Next Drop Code', readonly=True,
-                                  required=True),
-        'route_days_ids': fields.many2many('week.days',
-                                           'routedays_week_days_rel',
-                                           'route_id',
-                                           'route_days_id',
-                                           'Route Days'),
-    }
 
-    _defaults = {
-        'state': 'draft',
-        'next_dc': 1,
-    }
+    code = fields.Char('Code', size=32, required=True)
 
-    def set_active(self, cr, uid, ids, context=None):
-        return self.write(cr, uid, ids, {'state': 'active'}, context=context)
+    _sql_constraints = [
+        ('code_uniq', 'unique(code)', 'Zip Code can not be repeated.')
+    ]
 
-    def set_draft(self, cr, uid, ids, context=None):
-        return self.write(cr, uid, ids, {'state': 'draft'}, context=context)
 
-    def reset_drop_code(self, cr, uid, ids, context=None):
-        return self.write(cr, uid, ids, {'next_dc': '1'}, context=context)
+class route(models.Model):
+    _name = 'route'
+    _description = 'Route Model'
+    _rec_name = 'code'
+
+    code = fields.Char('Code', size=32, required=True)
+    name = fields.Char('Name', size=255, required=True)
+    # 'vehicle_id': fields.many2one('stock.vehicle', 'Vehicle',
+    #                               required=True),
+    state = fields.Selection([('draft', 'Draft'), ('active', 'Active')],
+                             string="State", readonly=True,
+                             default='draft')
+    next_dc = fields.Integer('Next Drop Code', readonly=True,
+                             required=True, default=1)
+    # 'route_days_ids': fields.many2many('week.days',
+    #                                    'routedays_week_days_rel',
+    #                                    'route_id',
+    #                                    'route_days_id',
+    #                                    'Route Days'),
+    day_id = fields.Many2one('week.days', 'Week_day', required=True)
+    type = fields.Selection([('auto_sale', 'Auto Sale'),
+                            ('comercial', 'Comercial'),
+                            ('delivery', 'Delivery'),
+                            ('ways', 'Ways'),
+                            ('other', 'Other')], 'Type', required=True,
+                            default='comercial')
+    comercial_id = fields.Many2one('res.users', 'Comercial', required=True)
+    zip_ids = fields.Many2many('route.zip', 'route_zip_codes_rel',
+                               'route_id', 'route_zip_id', 'Zip codes',
+                               required=True)
+
+    @api.onchange('zip_ids')
+    @api.multi
+    def onchange_zip_ids(self):
+        """
+        Check there is no same zip code in a route of same day and same type.
+        """
+        res = {}
+        warning = False
+        if self.zip_ids:
+            zip_codes = [x.code for x in self.zip_ids]
+            domain = [('day_id', '=', self.day_id.id),
+                      ('type', '=', self.type),
+                      ('code', '!=', self.code)]
+            route_objs = self.search(domain)
+            if route_objs:
+                not_check_more = False
+                for route in route_objs:
+                    for zip_c in route.zip_ids:
+                        if zip_c.code in zip_codes:
+                            warning = {
+                                'title': _('Warning!'),
+                                'message': _('The zip code %s is already \
+                                              assigned in the\
+                                              route %s. Change it or you can \
+                                              not save de \
+                                              route' % (zip_c.code,
+                                                        route.code))}
+                            not_check_more = True
+                    if not_check_more:
+                        break
+        if warning:
+            res['warning'] = warning
+        return res
+
+    @api.one
+    def write(self, vals):
+        """
+        Overwrite to Check there is no same zip code in a route of same day
+        and same type.
+        """
+        if vals.get('zip_ids', False):
+            zip_ids = vals['zip_ids'][0][2]
+            zip_objs = self.env['route.zip'].browse(zip_ids)
+            zip_codes = [x.code for x in zip_objs]
+            domain = [('day_id', '=', self.day_id.id),
+                      ('type', '=', self.type),
+                      ('code', '!=', self.code)]
+
+            route_objs = self.search(domain)
+            if route_objs:
+                for route_obj in route_objs:
+                    for zip_c in route_obj.zip_ids:
+                        if zip_c.code in zip_codes:
+                            raise except_orm(_('Error'),
+                                             _('The zip code %s is already \
+                                               assigned in the\
+                                               route %s. Change it or you can \
+                                               can not save th \
+                                               route' % (zip_c.code,
+                                                         route_obj.code)))
+        res = super(route, self).write(vals)
+        return res
+
+    @api.model
+    def create(self, vals):
+        if vals.get('zip_ids', False):
+            zip_ids = vals['zip_ids'][0][2]
+            zip_objs = self.env['route.zip'].browse(zip_ids)
+            zip_codes = [x.code for x in zip_objs]
+            domain = [('day_id', '=', vals['day_id']),
+                      ('type', '=', vals['type']),
+                      ('code', '!=', vals['code'])]
+
+            route_objs = self.search(domain)
+            if route_objs:
+                for route_obj in route_objs:
+                    for zip_c in route_obj.zip_ids:
+                        if zip_c.code in zip_codes:
+                            raise except_orm(_('Error'),
+                                             _('The zip code %s is already \
+                                               assigned in the\
+                                               route %s. Change it or you can \
+                                               can not save th \
+                                               route' % (zip_c.code,
+                                                         route_obj.code)))
+        res = super(route, self).create(vals)
+        return res
+
+    @api.multi
+    def set_active(self):
+        self.state = 'active'
+
+    @api.multi
+    def set_draft(self):
+        self.state = 'draft'
+
+    @api.multi
+    def reset_drop_code(self):
+        self.next_dc = 1
