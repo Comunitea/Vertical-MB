b030a663afddf560218ac952340e728d68bf1769
javierjcf <javier@pexego-magoi.pxgo.es>
2015-04-13 19:51:55 +0200

    [FIX]: midban_depot_stock: pequeño bug 2

--- midban_depot_stock/partner.py
+++ midban_depot_stock/partner.py
@@ -18,7 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-# from openerp.osv import osv, fields
+from openerp.osv import osv, fields
 from openerp.tools.translate import _
 from openerp import models, fields as fields2, api
 from openerp.exceptions import except_orm
