d653425d6239e8629ae169ccfeab605aecf230f2
javierjcf <javier@pexego-magoi.pxgo.es>
2015-04-13 19:45:04 +0200

    [FIX]: midban_depot_stock: pequeño bug

--- midban_depot_stock/partner.py
+++ midban_depot_stock/partner.py
@@ -166,97 +166,97 @@ class resPartner(models.Model):
                                       'Assigned Routes')
 
 
-# class res_partner(osv.Model):
-#     _inherit = 'res.partner'
-#     _columns = {
-#         'trans_route_id': fields.many2one('route', 'Transport Route',
-#                                           domain=[('state', '=', 'active')]),
-#     }
+class res_partner(osv.Model):
+    _inherit = 'res.partner'
+    _columns = {
+        'trans_route_id': fields.many2one('route', 'Transport Route',
+                                          domain=[('state', '=', 'active')]),
+    }
 
-#     @api.onchange('trans_route_id')
-#     @api.multi
-#     def onchange_route_id(self):
-#         """
-#         Check the delivery days of the customer and the route days.
-#         If no common days raise a warning
-#         raise a warning if some customer days are not in the transport route.
-#         """
-#         res = {}
-#         warning = False
-#         if not self.delivery_days_ids:
-#             warning = {
-#                 'title': _('Warning!'),
-#                 'message': _('There is no delivery days in this customer!'),
-#             }
-#         elif not self.trans_route_id.route_days_ids:
-#             warning = {
-#                 'title': _('Warning!'),
-#                 'message': _('There is no route days in the selected route!'),
-#             }
-#         else:
-#             partner_days = set(self.delivery_days_ids)
-#             route_days = set(self.trans_route_id.route_days_ids)
-#             common_days = partner_days & route_days
-#             if not common_days:
-#                 warning = {
-#                     'title': _('Warning!'),
-#                     'message': _('Selected delivery days are not in the \
-#                                  transport Route!'),
-#                 }
-#             else:
-#                 diff_days = list(partner_days - common_days)
-#                 diff_days = sorted(diff_days, key=lambda d: d.sequence)
-#                 diff_days_str = ""
-#                 for d in diff_days:
-#                     if not diff_days_str:
-#                         diff_days_str += d.name
-#                     else:
-#                         diff_days_str += "," + d.name
-#                 if diff_days_str:
-#                     warning = {
-#                         'title': _('Warning!'),
-#                         'message': _('The days %s are not in the selected \
-#                                       transport route') % diff_days_str,
-#                     }
-#         if warning:
-#             res['warning'] = warning
-#         return res
+    @api.onchange('trans_route_id')
+    @api.multi
+    def onchange_route_id(self):
+        """
+        Check the delivery days of the customer and the route days.
+        If no common days raise a warning
+        raise a warning if some customer days are not in the transport route.
+        """
+        res = {}
+        warning = False
+        if not self.delivery_days_ids:
+            warning = {
+                'title': _('Warning!'),
+                'message': _('There is no delivery days in this customer!'),
+            }
+        elif not self.trans_route_id.route_days_ids:
+            warning = {
+                'title': _('Warning!'),
+                'message': _('There is no route days in the selected route!'),
+            }
+        else:
+            partner_days = set(self.delivery_days_ids)
+            route_days = set(self.trans_route_id.route_days_ids)
+            common_days = partner_days & route_days
+            if not common_days:
+                warning = {
+                    'title': _('Warning!'),
+                    'message': _('Selected delivery days are not in the \
+                                 transport Route!'),
+                }
+            else:
+                diff_days = list(partner_days - common_days)
+                diff_days = sorted(diff_days, key=lambda d: d.sequence)
+                diff_days_str = ""
+                for d in diff_days:
+                    if not diff_days_str:
+                        diff_days_str += d.name
+                    else:
+                        diff_days_str += "," + d.name
+                if diff_days_str:
+                    warning = {
+                        'title': _('Warning!'),
+                        'message': _('The days %s are not in the selected \
+                                      transport route') % diff_days_str,
+                    }
+        if warning:
+            res['warning'] = warning
+        return res
 
-#     @api.onchange('delivery_days_ids')
-#     @api.multi
-#     def onchange_delivery_days_ids(self):
-#         """
-#         Check the delivery days of the customer and the route days.
-#         If no common days raise a warning
-#         raise a warning if some customer days are not in the transport route.
-#         """
-#         res = {}
-#         warning = False
-#         if self.trans_route_id and self.trans_route_id.route_days_ids:
-#             partner_days = set(self.delivery_days_ids)
-#             route_days = set(self.trans_route_id.route_days_ids)
-#             common_days = partner_days & route_days
-#             if not common_days:
-#                 warning = {
-#                     'title': _('Warning!'),
-#                     'message': _('Selected delivery days are not in the \
-#                                  transport Route!'),
-#                 }
-#             else:
-#                 diff_days = list(partner_days - common_days)
-#                 diff_days = sorted(diff_days, key=lambda d: d.sequence)
-#                 diff_days_str = ""
-#                 for d in diff_days:
-#                     if not diff_days_str:
-#                         diff_days_str += d.name
-#                     else:
-#                         diff_days_str += "," + d.name
-#                 if diff_days_str:
-#                     warning = {
-#                         'title': _('Warning!'),
-#                         'message': _('The days %s are not in the selected \
-#                                       transport route') % diff_days_str,
-#                     }
-#         if warning:
-#             res['warning'] = warning
-#         return res
+    @api.onchange('delivery_days_ids')
+    @api.multi
+    def onchange_delivery_days_ids(self):
+        """
+        Check the delivery days of the customer and the route days.
+        If no common days raise a warning
+        raise a warning if some customer days are not in the transport route.
+        """
+        res = {}
+        warning = False
+        if self.trans_route_id and self.trans_route_id.route_days_ids:
+            partner_days = set(self.delivery_days_ids)
+            route_days = set(self.trans_route_id.route_days_ids)
+            common_days = partner_days & route_days
+            if not common_days:
+                warning = {
+                    'title': _('Warning!'),
+                    'message': _('Selected delivery days are not in the \
+                                 transport Route!'),
+                }
+            else:
+                diff_days = list(partner_days - common_days)
+                diff_days = sorted(diff_days, key=lambda d: d.sequence)
+                diff_days_str = ""
+                for d in diff_days:
+                    if not diff_days_str:
+                        diff_days_str += d.name
+                    else:
+                        diff_days_str += "," + d.name
+                if diff_days_str:
+                    warning = {
+                        'title': _('Warning!'),
+                        'message': _('The days %s are not in the selected \
+                                      transport route') % diff_days_str,
+                    }
+        if warning:
+            res['warning'] = warning
+        return res
