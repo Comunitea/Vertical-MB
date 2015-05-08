b511a1c7b6d68e35a0375436d0844394ed57cb7a
Omar <omarcs7r@gmail.com>
2015-05-08 17:21:53 +0200

    [IMP] nan_partner_risk: Completamos la migración del módulo a la versión 8.0

--- nan_partner_risk/risk.py
+++ nan_partner_risk/risk.py
@@ -5,6 +5,8 @@
 #    Copyright (C) 2009 Albert Cervera i Areny (http://www.nan-tic.com). All Rights Reserved
 #    Copyright (c) 2011 Pexego Sistemas Informáticos. All Rights Reserved
 #                       Alberto Luengo Cabanillas <alberto@pexego.es>
+#    Copyright (c) 2015 Comunitea Servicios Tecnológicos. All Rights Reserved
+#                       Omar Castiñeira Saavedra <omar@comunitea.com>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -35,7 +37,7 @@ class sale_order_line(osv.osv):
         for line in self.browse(cr, uid, ids, context):
             # Calculate invoiced amount with taxes included.
             # Note that if a line is only partially invoiced we consider
-            # the invoiced amount 0. 
+            # the invoiced amount 0.
             # The problem is we can't easily know if the user changed amounts
             # once the invoice was created
             if line.invoiced:
@@ -43,7 +45,7 @@ class sale_order_line(osv.osv):
             else:
                 result[line.id] = 0.0
         return result
-				
+
     def _tax_amount(self, cr, uid, line):
         val = 0.0
         for c in self.pool.get('account.tax').compute_all(cr, uid, line.tax_id, line.price_unit * (1-(line.discount or 0.0)/100.0), line.product_uom_qty, line.order_id.partner_invoice_id.id, line.product_id, line.order_id.partner_id)['taxes']:
@@ -64,8 +66,8 @@ class sale_order(osv.osv):
             context=context
         result = super(sale_order,self).onchange_partner_id(cr, uid, ids, part, context=context)
         if part:
-            partner = self.pool.get('res.partner').browse(cr, uid, part)
-            if partner.available_risk < 0.0:
+            partner = self.pool.get('res.partner').browse(cr, uid, part).commercial_partner_id
+            if partner.credit_limit and partner.available_risk < 0.0:
                 result['warning'] = {
                     'title': _('Credit Limit Exceeded'),
                     'message': _('Warning: Credit Limit Exceeded.\n\nThis partner has a credit limit of %(limit).2f and already has a debt of %(debt).2f.') % {
@@ -90,14 +92,15 @@ class sale_order(osv.osv):
     _columns = {
         'amount_invoiced': fields.function(_amount_invoiced, method=True, string='Invoiced Amount', type='float'),
         'state': fields.selection([
-            ('draft', 'Quotation'),
+            ('draft', 'Draft Quotation'),
+            ('sent', 'Quotation Sent'),
+            ('cancel', 'Cancelled'),
             ('waiting_date', 'Waiting Schedule'),
-            ('manual', 'Manual In Progress'),
-            ('progress', 'In Progress'),
+            ('progress', 'Sales Order'),
+            ('manual', 'Sale to Invoice'),
             ('shipping_except', 'Shipping Exception'),
             ('invoice_except', 'Invoice Exception'),
             ('done', 'Done'),
-            ('cancel', 'Cancelled'),
             ('wait_risk', 'Waiting Risk Approval'),
             ], 'Order State', readonly=True, help=_("Gives the state of the quotation or sale order. The exception state is automatically set when a cancel operation occurs in the invoice validation (Invoice Exception) or in the packing list process (Shipping Exception). The 'Waiting Schedule' state is set when the invoice is confirmed but waiting for the scheduler to run on the date 'Date Ordered'."), select=True),
     }
@@ -118,41 +121,20 @@ class partner(osv.osv):
             if partner.property_account_payable:
                 accounts.append( partner.property_account_payable.id )
             line_ids = self.pool.get('account.move.line').search( cr, uid, [
-                ('partner_id','=',partner.id), 
-                ('account_id', 'in', accounts), 
-                ('reconcile_id','=',False), 
+                ('partner_id','=',partner.id),
+                ('account_id', 'in', accounts),
+                ('reconcile_id','=',False),
                 ('date_maturity','<',today),
-            ], context=context) 
+            ], context=context)
             # Those that have amount_residual == 0, will mean that they're circulating. The payment request has been sent
             # to the bank but have not yet been reconciled (or the date_maturity has not been reached).
             amount = 0.0
             for line in self.pool.get('account.move.line').browse( cr, uid, line_ids, context ):
-                #amount += -line.amount_residual
-                amount += line.debit - line.credit
-            res[partner.id] = amount
-        return res
-
-    def _circulating_amount(self, cr, uid, ids, name, arg, context=None):
-        res = {}
-        today = time.strftime('%Y-%m-%d')
-        for partner in self.browse(cr, uid, ids, context):
-            accounts = []
-            if partner.property_account_receivable:
-                accounts.append( partner.property_account_receivable.id )
-            if partner.property_account_payable:
-                accounts.append( partner.property_account_payable.id )
-            line_ids = self.pool.get('account.move.line').search( cr, uid, [
-                ('partner_id','=',partner.id), 
-                ('account_id', 'in', accounts), 
-                ('reconcile_id','=',False), 
-                '|', ('date_maturity','>=',today), ('date_maturity','=',False)
-            ], context=context) 
-            # Those that have amount_residual == 0, will mean that they're circulating. The payment request has been sent
-            # to the bank but have not yet been reconciled (or the date_maturity has not been reached).
-            amount = 0.0
-            for line in self.pool.get('account.move.line').browse( cr, uid, line_ids, context ):
-                #amount += line.debit - line.credit
-                amount += line.debit - line.credit + line.amount_residual
+                if line.currency_id:
+                    sign = line.amount_currency < 0 and -1 or 1
+                else:
+                    sign = (line.debit - line.credit) < 0 and -1 or 1
+                amount += sign * line.amount_residual
             res[partner.id] = amount
         return res
 
@@ -166,17 +148,20 @@ class partner(osv.osv):
             if partner.property_account_payable:
                 accounts.append( partner.property_account_payable.id )
             line_ids = self.pool.get('account.move.line').search( cr, uid, [
-                ('partner_id','=',partner.id), 
-                ('account_id', 'in', accounts), 
-                ('reconcile_id','=',False), 
+                ('partner_id','=',partner.id),
+                ('account_id', 'in', accounts),
+                ('reconcile_id','=',False),
                 '|', ('date_maturity','>=',today), ('date_maturity','=',False)
-            ], context=context) 
+            ], context=context)
             # Those that have amount_residual == 0, will mean that they're circulating. The payment request has been sent
             # to the bank but have not yet been reconciled (or the date_maturity has not been reached).
             amount = 0.0
             for line in self.pool.get('account.move.line').browse( cr, uid, line_ids, context ):
-                #amount += line.debit - line.credit
-                amount += -line.amount_residual
+                if line.currency_id:
+                    sign = line.amount_currency < 0 and -1 or 1
+                else:
+                    sign = (line.debit - line.credit) < 0 and -1 or 1
+                amount += sign * line.amount_residual
             res[partner.id] = amount
         return res
 
@@ -185,14 +170,14 @@ class partner(osv.osv):
         today = time.strftime('%Y-%m-%d')
         for id in ids:
             invids = self.pool.get('account.invoice').search( cr, uid, [
-                ('partner_id','=',id), 
-                ('state','=','draft'), 
+                ('partner_id','child_of',[id]),
+                ('state','=','draft'),
                 '|', ('date_due','>=',today), ('date_due','=',False)
             ], context=context )
             val = 0.0
             for invoice in self.pool.get('account.invoice').browse( cr, uid, invids, context ):
-                # Note that even if the invoice is in 'draft' state it can have an account.move because it 
-                # may have been validated and brought back to draft. Here we'll only consider invoices with 
+                # Note that even if the invoice is in 'draft' state it can have an account.move because it
+                # may have been validated and brought back to draft. Here we'll only consider invoices with
                 # NO account.move as those will be added in other fields.
                 if invoice.move_id:
                     continue
@@ -207,7 +192,7 @@ class partner(osv.osv):
         res = {}
         for id in ids:
             sids = self.pool.get('sale.order').search( cr, uid, [
-                ('partner_id','=',id),
+                ('partner_id','child_of',[id]),
                 ('state','not in',['draft','cancel','wait_risk'])
             ], context=context )
             total = 0.0
@@ -220,11 +205,10 @@ class partner(osv.osv):
         res = {}
         for partner in self.browse( cr, uid, ids, context ):
             pending_orders = partner.pending_orders_amount or 0.0
-            circulating = partner.circulating_amount or 0.0
             unpayed = partner.unpayed_amount or 0.0
             pending = partner.pending_amount or 0.0
             draft_invoices = partner.draft_invoices_amount or 0.0
-            res[partner.id] = pending_orders + circulating + unpayed + pending + draft_invoices
+            res[partner.id] = pending_orders + unpayed + pending + draft_invoices
         return res
 
     def _available_risk(self, cr, uid, ids, name, arg, context=None):
@@ -246,7 +230,6 @@ class partner(osv.osv):
         'unpayed_amount': fields.function(_unpayed_amount, method=True, string=_('Expired Unpaid Payments'), type='float'),
         'pending_amount': fields.function(_pending_amount, method=True, string=_('Unexpired Pending Payments'), type='float'),
         'draft_invoices_amount': fields.function(_draft_invoices_amount, method=True, string=_('Draft Invoices'), type='float'),
-        'circulating_amount': fields.function(_circulating_amount, method=True, string=_('Payments Sent to Bank'), type='float'),
         'pending_orders_amount': fields.function(_pending_orders_amount, method=True, string=_('Uninvoiced Orders'), type='float'),
         'total_debt': fields.function(_total_debt, method=True, string=_('Total Debt'), type='float'),
         'available_risk': fields.function(_available_risk, method=True, string=_('Available Credit'), type='float'),
