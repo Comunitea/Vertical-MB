3fc850b752f2d393cd0530527d34a6a5cfaf0977
Kiko <kiko@comunitea.com>
2015-08-12 10:14:32 +0200

    [BUG].process_preorder. calculo de unidades minimas de proveedor + referencias a vncersiones por proveedor

--- purchase_preorder/purchase.py
+++ purchase_preorder/purchase.py
@@ -28,29 +28,46 @@ class purchase_order_line(osv.Model):
     def _calc_uoms(self, cr, uid, ids, field_name, arg, context=None):
         res = {}
         mod_obj = self.pool.get('ir.model.data')
+
         for line in self.browse(cr, uid, ids, context=context):
             res[line.id] = {
                 'palets': 0.0,
                 'mantles': 0.0,
                 'boxes': 0.0,
             }
+            un_ca = ca_ma = ma_pa = 1.0
             boxes = mantles = palets = 0.0
+
             unit_uom = mod_obj.get_object_reference(cr,
                                                     uid,
                                                     'product',
                                                     'product_uom_unit')
-            unit_id = unit_uom and unit_uom[1] or False
-            if line.product_qty and line.product_uom.id == unit_id:
-                un_ca = line.product_id.supplier_un_ca
-                ca_ma = line.product_id.supplier_ca_ma
-                ma_pa = line.product_id.supplier_ma_pa
-                boxes = round(un_ca and (line.product_qty / un_ca) or 0.0, 2)
-                mantles = round(ca_ma and (boxes / ca_ma) or 0.0, 2)
-                palets = round(ma_pa and (mantles / ma_pa) or 0.0, 2)
+            supplier_id = line.order_id.partner_id
+            product_id = line.product_id
 
-            res[line.id]['boxes'] = boxes
-            res[line.id]['mantles'] = mantles
-            res[line.id]['palets'] = palets
+            suppinfo = self.pool.get('product.supplierinfo').\
+                search(cr, uid,[('product_tmpl_id', '=', product_id.id)
+                , ('name', '=', supplier_id.id)])
+            supp = self.pool.get('product.supplierinfo').browse(cr, uid, suppinfo)
+            if supp:
+                un_ca = supp.supp_un_ca
+                ca_ma = supp.supp_ca_ma
+                ma_pa = supp.supp_ma_pa
+
+            #Buscamos cajas
+            conv = product_id.get_purchase_unit_conversions(line.product_uoc_qty, line.product_uoc, supplier_id.id)
+
+            # unit_id = unit_uom and unit_uom[1] or False
+            # if line.product_qty and line.product_uom.id == unit_id:
+            #     units = line.product_qty
+            #     boxes = round(un_ca and (units / un_ca) or 0.0, 2)
+            #     mantles = round(ca_ma and (boxes / ca_ma) or 0.0, 2)
+            #     palets = round(ma_pa and (mantles / ma_pa) or 0.0, 2)
+
+            res[line.id]['boxes'] = conv['box']
+            res[line.id]['mantles'] = round(ca_ma and (conv['box'] / ca_ma) or 0.0, 2)
+            res[line.id]['palets'] = round(ma_pa and (conv['box'] / (ca_ma * ma_pa)) or 0.0, 2)
+            import pdb; pdb.set_trace()
         return res
 
     _columns = {
