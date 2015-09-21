6070a6b3b98aec46299f3ed243afa0a18b6f291e
CarlosLombardia <carlos@comunitea.com>
2015-09-21 17:34:48 +0200

    [REF]purchase_preorder: sacados de la carpeta wizard los modelos

--- purchase_preorder/__init__.py
+++ purchase_preorder/__init__.py
@@ -18,5 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import wizard
+# import wizard
 import purchase
+import purchase_preorder
+import products_supplier
