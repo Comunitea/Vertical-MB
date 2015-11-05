fcf95c1411dc7efcfdb119f3d27bc23ea2f39d2a
javierjcf <javierjcf@gmail.com>
2015-11-05 16:28:08 +0100

    [IMP]midban_depot_stock: Al validar rutas con el asistente comprobar disponibilidad y separar albaranes por cámaras 1

--- midban_depot_stock/wizard/validate_routes.py
+++ midban_depot_stock/wizard/validate_routes.py
@@ -19,6 +19,8 @@
 #
 ##############################################################################
 from openerp import models, fields, api
+from openerp.exceptions import except_orm
+from openerp.tools.translate import _
 
 
 class ValidateRoutes(models.TransientModel):
@@ -27,6 +29,88 @@ class ValidateRoutes(models.TransientModel):
 
     @api.multi
     def validate(self):
-        pickings = self.env['stock.picking'].browse(self.env.context['active_ids'])
-        pickings.write({'validated': True})
+        active_ids = self.env.context['active_ids']
+        out_pickings = self.env['stock.picking'].browse(active_ids)
 
+        pick_pickings = self._get_pickings_from_outs(out_pickings)
+
+        unasigned_picks_lst = []
+        for pick in pick_pickings:
+            if not pick.route_detail_id:
+                raise except_orm(_('Error'),
+                                 _('Picking %s without has not route detatl \
+                                   assigned' % pick.name))
+
+            # Put unassigned moves inside a new picking
+            unassigned_pick = False
+            for move in pick.move_lines:
+                move.action_assign()
+                if move.state != 'assigned':
+                    if not unassigned_pick:
+                        copy_values = {'move_lines': [],
+                                       'pack_operation_ids': [],
+                                       'route_detail_id': False,
+                                       'group_id': pick.group_id.id}
+                        unassigned_pick = pick.copy(copy_values)
+                        unasigned_picks_lst.append(unassigned_pick)
+                    move.picking_id = unassigned_pick.id
+
+            # Create as many picks as cameras involved and validate_it.
+            picks_by_cam = self._split_pick_by_cameras(pick)
+            picks_by_cam.write({'validated': True})
+            out_pickings.write({'validated': True})
+        return
+
+    def _get_pickings_from_outs(self, out_pickings):
+        res = self.env['stock.picking']
+        for pick in out_pickings:
+            if not (pick.sale_id and pick.picking_type_code == 'outgoing'):
+                raise except_orm(_('Error'),
+                                 _('Picking %s wmust be outgoing type and  \
+                                   related with a sale' % pick.name))
+
+            if not pick.route_detail_id:
+                raise except_orm(_('Error'),
+                                 _('Picking %s without has not route detatl \
+                                   assigned' % pick.name))
+            if pick.group_id:
+                domain = [('id', '!=', pick.id),
+                          ('group_id', '=', pick.group_id.id),
+                          ('picking_type_code', '!=', 'outgoing')]
+                pick_objs = self.env['stock.picking'].search(domain)
+                for p in pick_objs:
+                    res += p
+        return res
+
+    def _split_pick_by_cameras(self, pick):
+        splited_picks = self.env['stock.picking']
+        if pick.state != 'assigned':
+            raise except_orm(_('Error'),
+                             _('Picking %s is not assigned' % pick.name))
+        pick.do_prepare_partial()
+        moves_by_cam = {}  # Moves grouped by camera
+        for op in pick.pack_operation_ids:
+            camera_loc = op.location_id.get_camera()
+            if camera_loc not in moves_by_cam:
+                moves_by_cam[camera_loc] = []
+            move_objs = list(
+                set([x.move_id for x in op.linked_move_operation_ids]))
+            moves_by_cam[camera_loc].extend(move_objs)
+
+        first = True
+
+        for cam in moves_by_cam:
+            if first:  # Skip first moves, we get the original picking
+                first = False
+                pick.camera_id = cam
+                continue
+            copy_values = {'move_lines': [],
+                           'pack_operation_ids': [],
+                           'route_detail_id': pick.route_detail_id.id,
+                           'group_id': pick.group_id.id}
+            new_pick = pick.copy(copy_values)
+            for move in moves_by_cam[cam]:
+                move.picking_id = new_pick  # Assign move to new pick
+            new_pick.camera_id = cam  # Write the camera
+            splited_picks += new_pick
+        return splited_picks
