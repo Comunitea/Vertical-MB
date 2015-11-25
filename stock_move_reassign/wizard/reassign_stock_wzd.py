61b62f40408286f25da48d75a0e9b5e706bdeb16
javierjcf <javierjcf@gmail.com>
2015-11-25 02:22:15 +0100

    [IMP]: Mejora considerable en el asistente de reasignación de mercancía

--- stock_move_reassign/wizard/reassign_stock_wzd.py
+++ stock_move_reassign/wizard/reassign_stock_wzd.py
@@ -39,11 +39,14 @@ class ReassignStockWzd(models.TransientModel):
         if self.env.context.get('active_ids', []):
             move_obj = self.env["stock.move"]
             move = move_obj.browse(self.env.context['active_ids'][0])
-            assigned_moves = move_obj.search([('product_id', '=',
-                                               move.product_id.id),
-                                              ('state', '=', 'assigned'),
-                                              ('location_id', '=',
-                                               move.location_id.id)])
+            domain = [
+                ('id', '!=', move.id),
+                ('product_id', '=', move.product_id.id),
+                '|',
+                ('state', '=', 'assigned'),
+                ('partially_available', '=', True),
+                ('location_id', '=', move.location_id.id)]
+            assigned_moves = move_obj.search(domain)
             res['line_ids'] = []
             for amove in assigned_moves:
                 line = {
@@ -52,9 +55,11 @@ class ReassignStockWzd(models.TransientModel):
                     'product_id': amove.product_id.id,
                     'product_qty': amove.product_uom_qty,
                     'uom_id': amove.product_uom.id,
+                    'assigned_qty': amove.reserved_availability,
                     'location_id': amove.location_id.id,
                     'partner_id': amove.partner_id.id,
-                    'state': amove.state
+                    'state': amove.state,
+                    'reassign_qty': move.remaining_qty
                 }
                 res['line_ids'].append(line)
         return res
@@ -62,7 +67,6 @@ class ReassignStockWzd(models.TransientModel):
     def _reassign_quants_for_move(self, assigned_move, reassign_qty):
         t_move = self.env["stock.move"]
         t_quant = self.env["stock.quant"]
-        import ipdb; ipdb.set_trace()
         to_assign_move = t_move.browse(self.env.context['active_ids'][0])
         for q in assigned_move.reserved_quant_ids:
             if reassign_qty:
@@ -75,56 +79,58 @@ class ReassignStockWzd(models.TransientModel):
                     reassign_qty = 0
         return
 
-    def _change_operations_for_move(self, assigned_move, reassign_qty):
-        import ipdb; ipdb.set_trace()
-        for link in assigned_move.linked_move_operation_ids:
-            op = link.operation_id
-            op_qty = op.product_qty if op.product_id else \
-                op.package_id.packed_qty
-            if op_qty > reassign_qty:
-                prod = op.operation_product_id
-                new_qty = op_qty - reassign_qty
-                new_uos_qty = prod.uom_qty_to_uos_qty(new_qty, op.uos_id.id)
-                vals = {
-                    'product_id': prod.id,
-                    'product_qty': new_qty,
-                    'product_uom_id': prod.uom_id.id,
-                    'lot_id': op.package_id.packed_lot_id.id,
-                    'uos_qty': new_uos_qty,
-                }
-                op.write(vals)
-                break
-            else:
-                op.unlink()
-                reassign_qty -= op.qty
-        return
-
-    def _put_move_in_validated_picking(self):
-        move_obj = self.env["stock.move"]
-        wzd_move = move_obj.browse(self.env.context['active_ids'][0])
-        import ipdb; ipdb.set_trace()
-        return
+    # def _change_operations_for_move(self, assigned_move, reassign_qty):
+    #     for link in assigned_move.linked_move_operation_ids:
+    #         op = link.operation_id
+    #         op_qty = op.product_qty if op.product_id else \
+    #             op.package_id.packed_qty
+    #         if op_qty > reassign_qty:
+    #             prod = op.operation_product_id
+    #             new_qty = op_qty - reassign_qty
+    #             new_uos_qty = prod.uom_qty_to_uos_qty(new_qty, op.uos_id.id)
+    #             vals = {
+    #                 'product_id': prod.id,
+    #                 'product_qty': new_qty,
+    #                 'product_uom_id': prod.uom_id.id,
+    #                 'lot_id': op.package_id.packed_lot_id.id,
+    #                 'uos_qty': new_uos_qty,
+    #             }
+    #             op.write(vals)
+    #             break
+    #         else:
+    #             op.unlink()
+    #             reassign_qty -= op.qty
+    #     return
 
     @api.multi
     def unreserve(self):
         self.ensure_one()
         wzd = self
-        import ipdb; ipdb.set_trace()
         t_move = self.env["stock.move"]
         to_assign_move = t_move.browse(self.env.context['active_ids'][0])
         for line in wzd.line_ids:
             if not line.reassign_qty:
                 continue
-            if line.reassign_qty > line.product_qty:
+            if line.reassign_qty > line.assigned_qty:
                 # line.move_id.do_unreserve()
                 raise except_orm(_('Error'),
                                  _('Quantity to reassign greater than \
                                    unnasigned quantity for picking \
                                    %s' % line.picking_id.name))
             self._reassign_quants_for_move(line.move_id, line.reassign_qty)
-            self._change_operations_for_move(line.move_id, line.reassign_qty)
-            # self._create_operatio(line.move_id, line.reassign_qty)
-            self._put_move_in_validated_picking()
+            line.move_id.recalculate_move_state()
+            line.move_id.recalculate_move_state()
+            # self._change_operations_for_move(line.move_id, line.reassign_qty)
+            line.move_id.picking_id.delete_picking_package_operations()
+            line.move_id.picking_id.do_prepare_partial()
+            to_assign_move.picking_id.delete_picking_package_operations()
+            to_assign_move.picking_id.do_prepare_partial()
+
+            line.move_id.picking_id.delete_picking_package_operations()
+            line.move_id.picking_id.do_prepare_partial()
+            # self._put_move_in_validated_picking()
+            to_assign_move.picking_id.delete_picking_package_operations()
+            to_assign_move.picking_id.do_prepare_partial()
 
 
         return True
@@ -152,6 +158,10 @@ class ReassignStockLineWzd(models.TransientModel):
                               "State", readonly=True)
     wzd_id = fields.Many2one("reassign.stock.wzd", "Wzd")
     reassign_qty = fields.Float('Qty to treassign',
+                                digits=dp.get_precision
+                                ('Product Unit of Measure'),
+                                default=0.0)
+    assigned_qty = fields.Float('Qty to treassign',
                                 digits=dp.get_precision
                                 ('Product Unit of Measure'),
                                 default=0.0)
