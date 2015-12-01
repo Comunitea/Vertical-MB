9e29861bc1a1e66a8037c1e0a99d2bcf07bfe9fd
javierjcf <javierjcf@gmail.com>
2015-12-01 16:41:49 +0100

    [FIX]midban_depot_stock: Bug al cancelar tarea por no llamar bien al super

--- midban_depot_stock/sale.py
+++ midban_depot_stock/sale.py
@@ -207,7 +207,7 @@ class sale_order(osv.Model):
                 if pick.state not in ['draft', 'cancel', 'done'] and pick.validated_state != 'no_validated':
                     raise except_orm(_('Error'), _('The related pick %s has been validated or load confirmed. You must'
                                                    ' cancell manually first' % pick.name))
-        return super(sale_order).action_cancel()
+        return super(sale_order, self).action_cancel()
 
     @api.multi
     def write(self, vals):
