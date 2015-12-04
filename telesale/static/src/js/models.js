0b34efdf8e6bbbd8f76976def7988d109cc9a539
javierjcf <javierjcf@gmail.com>
2015-12-04 17:08:04 +0100

    [FIX]telesale: correción de los precios en el subtotal al recuperar

--- telesale/static/src/js/models.js
+++ telesale/static/src/js/models.js
@@ -322,7 +322,8 @@ function openerp_ts_models(instance, module){
                                  unit:line.product_uom[1],
                                  qty:line.product_uom_qty,
                                  pvp:my_round(line.price_unit,2), //TODO poner precio del producto???
-                                 total: my_round(line.current_pvp ? (line.product_uom_qty * line.current_pvp) * (1 - line.discount /100) : 0 ,2),
+//                                 total: my_round(line.current_pvp ? (line.product_uom_qty * line.current_pvp) * (1 - line.discount /100) : 0 ,2),
+                                 total: my_round(line.product_uom_qty * line.price_unit * (1 - line.discount /100)),
                                 //  discount: my_round( ((line.pvp_ref == 0) ? 0: 1 - (line.price_unit / line.pvp_ref)), 2 ),
                                  discount: my_round(line.discount, 2) || 0.0,
                                  weight: my_round(prod_obj.weight * line.product_uom_qty,2),
@@ -909,7 +910,8 @@ function openerp_ts_models(instance, module){
                                  unit:prod_obj.uom_id[1] || line.product_uom[1], //current product unit
                                  qty:my_round(l_qty), //order line qty
                                  pvp: my_round(line.current_pvp ? line.current_pvp : 0, 2), //current pvp
-                                 total: my_round(line.current_pvp ? (line.product_uom_qty * line.current_pvp) * (1 - line.discount /100) : 0 ,2),
+//                                 total: my_round(line.current_pvp ? (line.product_uom_qty * line.current_pvp) * (1 - line.discount /100) : 0 ,2),
+                                 total: my_round(line.product_uom_qty * line.price_unit * (1 - line.discount /100)),
                                  discount: my_round( line.discount || 0.0, 2 ),
                                  weight: my_round(line.product_uom_qty * prod_obj.weight,2),
                                  margin: my_round(( (line.current_pvp != 0 && prod_obj.product_class == "normal") ? ( (line.current_pvp - prod_obj.standard_price) / line.current_pvp)  : 0 ), 2),
